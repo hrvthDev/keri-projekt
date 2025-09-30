5656ed73779f453585596ce25efbe7d04df35729
Horváth Dávid <Horvth Dvid@katolikuskeri.hu>
2025-09-30 12:00:28 +0200

    Állományok létrehozása, tartalom hozzáaddása, képek hozzáaddása.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 function toggleBar() {
-    const dropdownBtn = document.getElementById("dropdownBtn");
-    const dropdownMenu = document.getElementById("dropdownMenu");
+  const dropdownBtn = document.getElementById("dropdownBtn");
+  const dropdownMenu = document.getElementById("dropdownMenu");
 
-    dropdownBtn.addEventListener("click", () => {
-        dropdownMenu.classList.toggle("show");
-    });
+  dropdownBtn.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
+  });
 }
 
+function scrollBar() {
+
+
+  window.addEventListener("scroll", () => {
+
+    const header = document.getElementById("header");
+
+    if (window.scrollY > 90) {
+      header.classList.add("fix");
+    } else {
+      header.classList.remove("fix");
+    }
+
+
+  })
+
+
+
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  scrollBar();
   toggleBar();
 });
+
+
+
